d05ba82a8855c341726870719bbee5da5c87d379
Lucio Lastra Paulos <luciolastra@gmail.com>
2024-11-13 17:18:30 -0300

    Se agrega al script de instalacion de la AMI el paquete audit.

--- 06-instalar-paquetes/instalar-paquetes-AMI.sh
+++ 06-instalar-paquetes/instalar-paquetes-AMI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Instalar paquetes sugeridos por Lynis para mejorar el hardening.
-apt install -y libpam-tmpdir apt-listbugs needrestart debsums apt-show-versions fail2ban aide acct unattended-upgrades
+apt install -y libpam-tmpdir apt-listbugs needrestart debsums apt-show-versions fail2ban aide acct unattended-upgrades auditd audispd-plugins
